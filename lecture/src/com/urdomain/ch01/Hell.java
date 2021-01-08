df8950bea0626357ae1411e391b6a359d9c9472c
hsk0922 <hsk0922@naver.com>
2021-01-08 11:58:22 +0900

    a

--- lecture/src/com/urdomain/ch01/Hell.java
+++ lecture/src/com/urdomain/ch01/Hell.java
@@ -3,7 +3,7 @@ package com.urdomain.ch01;
 public class Hell {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Welcome to the Hell");
+		System.out.println("Wow, Hello, Welcome to the Hell");
 
 	}
 
