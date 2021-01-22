17a35e8ab0d832aa57b411decb1271963432f2d9
hsk0922 <hsk0922@naver.com>
2021-01-22 10:23:31 +0900

    2021-01-22

---
+++ lecture/src/com/urdomain/practice/CommonReference.java
@@ -0,0 +1,28 @@
+package com.urdomain.practice;
+
+public class CommonReference {
+	public static String boardName; // �Խ��� �̸�
+	public static String postTitle; // �Խù� ����
+	public static String writer; // �Խù� �ۼ���
+	public static String postedDate; // �Խù� �ۼ���
+	public static int postNumber; // �Խù� ��ȣ
+	public static int comments; // �Խù� ��ۼ�
+	public static int numberOfViews; // �Խù� ��ȸ��
+
+	public void movePost() {
+		// �Խñ۷� �̵�
+	}
+
+	public void moveComments() {
+		// �Խñ� �ȿ� �����ġ�� �ٷ� �̵�
+	}
+
+	public void moveWriter() {
+		// �ۼ��� ������ �̵� (or Ŭ���� �ۼ��� �Խñ� ,1:1ä��, ����������� �����޴� �����ֱ�)
+	}
+
+	public void writing() {
+		// �۾���
+	}
+
+}
